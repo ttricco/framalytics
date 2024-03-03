44e15d33d20d0b256133f1f51cc23d1b4470769b
TheFatalBlazer <jabillard@mun.ca>
2024-03-03 16:50:06 -0330

    Updated the visualizer to be a class.
    Added adjusting variables that the user can use to adjust the dimensions of the display.
    
    Changed variables names to accurately identify their purpose.

--- framalytics/FRAM Visualizer.py
+++ framalytics/FRAM Visualizer.py
@@ -2,162 +2,209 @@ from read_xfmv import *
 from xfmv_parser import *
 import networkx as nx
 import matplotlib
-matplotlib.use('WebAgg')
-
-import matplotlib.pyplot as plt
 import textwrap
+from matplotlib.bezier import BezierSegment
 
-def function_nodes():
-    # Gets FRAM data in tuple form.
-    # Tuple[0] = Function Element Data
-    # Tuple[1] = Input Element Info for function connections
-    # Tuple[2] = Aspect Element Data
-    fram_data = read_xfmv("FRAM model-Stroke care system.xfmv")
+#matplotlib.use('WebAgg')
+import matplotlib.pyplot as plt
 
 
-    # Scatter plot sample for FRAM replication
 
-    # X,Y coordinates of Nodes and the color and labels.
-    test_x = []
-    test_y = []
-    test_label = []
-    test_color = []
+class Visualizer:
 
+    def __init__(self, file, figsize_x=1200, figsize_y=600, dpi=150, backend=None):
+        self.file = file
+        self.figsize_x = figsize_x  # Controls x-dimension of plot display
+        self.figsize_y = figsize_y  # Controls y-dimension of plot display
+        self.dpi = dpi  # Controls size of plot display.
 
-    for index, row in fram_data[0].iterrows():
-        test_x.append(float(row.x))
-        test_y.append(float(row.y))
-        test_label.append(row.IDName)
+        self.fram_data = None
+        self.node_x_coords = []
+        self.node_y_coords = []
+        self.backend = backend  # Should be WebAgg or nbAgg (needs to be tested
 
-        #Converts node 32bit integer color to hexadecimal
-        if type(row.color) == type(None):
-            test_color.append("black")
-        else:
-            color = hex(int(row.color))
-            color = color[2:]
-            while(len(color)<6):
-                color = "0"+color
-            color = "#"+color
-            test_color.append(color)
+        if self.backend != None:
+            matplotlib.use(backend)
 
-    # Makes figure size
+        # Sets visual dimensions for plot. (Hard coded currently).
+        px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
 
+        # plt.figure(figsize=(2433 * px, 657 * px), dpi=150)
 
-    # Plots nodes.
-    plt.scatter(test_x, test_y, label=test_label, marker='H', s=1500, facecolors='white', edgecolors=test_color, lw=5, zorder=2)
+        # Creates the figure dimensions and size.
+        plt.figure(figsize=(self.figsize_x * px, self.figsize_y * px), dpi=self.dpi)
+        plt.gca().invert_yaxis()
+        plt.axis("off")
 
+    def function_nodes(self):
+        # Gets FRAM data in tuple form.
+        self.fram_data = read_xfmv(self.file)
 
-    # Makes multi-line labels
-    for index, row in fram_data[0].iterrows():
-        wrapped_label = textwrap.fill(row.IDName,width=15)
-        plt.annotate(wrapped_label,(float(row.x),float(row.y)), ha='center', va='center', fontsize=5)
+        # X,Y coordinates of Nodes and the color and labels.
+        self.node_x_coords = []
+        self.node_y_coords = []
 
-    return [fram_data,test_x,test_y]
+        # Gets the labels, colors, face colors and line width of each node
+        node_labels = []
+        node_colors = []
+        node_facecolors = []  # Array of face colors
+        node_lw = []  # Array of line widths (borders)
 
+        # Will store true or false values for each feature (numbered by index) to determine if
+        # they have inputs and/or outputs. Will determine if the facecolor is 'white" or very light grey '#F3F3F3'
+        node_toFn = []
+        node_outputFn = []
 
-def aspects(fram_data,test_x,test_y):
-    # Aspect creation
-    aspects_x = []
-    aspects_y = []
-    aspect_labels = []
-    aspects_per_function = [] #S tores aspect coordinates per function in order of FunctionID (Function IDNr).
+        # For loop that determines each node's x and y coordinates, label, and colors.
+        for index, row in self.fram_data[0].iterrows():
+            self.node_x_coords.append(float(row.x))
+            self.node_y_coords.append(float(row.y))
+            node_labels.append(row.IDName)
 
-    # Iterates through x and y location of each node to get aspect locations and connections (plus labelling)
-    for x, y in zip(test_x,test_y):
-        # T,C,I,O,P,R
-        aspects_per_function.append([[x-25,y-35],[x+25,y-35],[x-44,y],[x+44,y],[x-25,y+35],[x+25,y+35]])
+            # Converts node 32bit integer color to hexadecimal
+            if type(row.color) == type(None):
+                node_colors.append("black")
+                node_lw.append(0.5)
 
-        # Top left (Note, y-axis is inverted) (T)
-        aspects_x.append(x-25)
-        aspects_y.append(y-35)
-        aspect_labels.append("T")
-        plt.plot([x-25,x],[y-35,y],color='black',zorder=1)  #Lower zorder prevents lines from going through nodes
+            else:
+                color = hex(int(row.color))
+                color = color[2:]
+                while len(color) < 6:
+                    color = "0"+color
+                color = "#"+color
+                node_colors.append(color)
+                node_lw.append(3)
 
-        # Top right (C)
-        aspects_x.append(x+25)
-        aspects_y.append(y-35)
-        aspect_labels.append("C")
-        plt.plot([x+25, x], [y-35, y], color='black',zorder=1)
+            node_toFn.append(False)
+            node_outputFn.append(False)
 
-        # Mid left (I)
-        aspects_x.append(x-44)
-        aspects_y.append(y)
-        aspect_labels.append("I")
-        plt.plot([x-44, x], [y, y], color='black',zorder=1)
+        # Determines which nodes (ordered by the functions IDNr = index) have outputs and/or inputs.
+        for i in self.fram_data[2].toFn:
+            if i != None:
+                node_toFn[int(i)] = True
 
-        # Mid right (O)
-        aspects_x.append(x+44)
-        aspects_y.append(y)
-        aspect_labels.append("O")
-        plt.plot([x+44, x], [y, y], color='black',zorder=1)
+        for i in self.fram_data[2].outputFn:
+            if i != None:
+                node_outputFn[int(i)] = True
 
-        # Bottom left (P)
-        aspects_x.append(x-25)
-        aspects_y.append(y+35)
-        aspect_labels.append("P")
-        plt.plot([x-25, x], [y+35, y], color='black',zorder=1)
+        for i in range(len(node_toFn)):
+            # If a function/node has both an input and output, the face color is white.
+            if (node_toFn[i]) and (node_outputFn[i]):
+                node_facecolors.append('white')
 
-        # Bottom right (R)
-        aspects_x.append(x+25)
-        aspects_y.append(y+35)
-        aspect_labels.append("R")
-        plt.plot([x+25, x], [y+35, y], color='black',zorder=1)
+            # If a function/node does not have both an input and output, the face color is very light grey.
+            else:
+                node_facecolors.append('#F3F3F3')
 
-    # Adds aspects to each node
-    plt.scatter(aspects_x, aspects_y, s=30, facecolors='white', edgecolors='black', lw=0.5,zorder=2)
+        # Plots nodes.
+        plt.scatter(self.node_x_coords, self.node_y_coords, label=node_labels, marker='H', s=1500, facecolors=node_facecolors, edgecolors=node_colors, lw=node_lw, zorder=3)
 
-    # Applies proper labels to scatter plot
-    for label, x, y in zip(aspect_labels, aspects_x, aspects_y):
-        wrapped_label = textwrap.fill(label, break_long_words=False,width=15)
-        plt.annotate(wrapped_label, (x, y), ha='center', va='center', fontsize=5)
+        # Makes multi-line labels
+        for index, row in self.fram_data[0].iterrows():
+            wrapped_label = textwrap.fill(row.IDName, width=13)
+            plt.annotate(wrapped_label, (float(row.x), float(row.y)), ha='center', va='center', fontsize=4)
 
-def bezier_curves(fram_data):
-    #BEZIER CURVE
+    def aspects(self):
+        # Aspect creation
+        aspects_x = []
+        aspects_y = []
+        aspect_labels = []
+        aspects_per_function = []  # Stores aspect coordinates per function in order of FunctionID (Function IDNr).
 
-    import matplotlib.pyplot as plt
-    from matplotlib.bezier import BezierSegment
+        # Iterates through x and y location of each node to get aspect locations and connections (plus labelling)
+        for x, y in zip(self.node_x_coords, self.node_y_coords):
+            # T,C,I,O,P,R  coordinate ordering
+            aspects_per_function.append([[x-23, y-35], [x+23, y-35], [x-44, y], [x+44, y], [x-23, y+35], [x+23, y+35]])
 
-    for i in fram_data[2].Curve:
-        # Curve coordinates
-        a = i.split("|")
-        # Must be in this order!
-        control_points = [(float(a[2]), float(a[3])), (float(a[4]), float(a[5])), (float(a[8]), float(a[9])), (float(a[6]), float(a[7])), (float(a[0]), float(a[1]))]
-        bezier_segment = BezierSegment(control_points)
+            # EACH ASPECT IS INDIVIDUALLY PLOTTED TO GENERATE A LINE BETWEEN ITSELF AND ITS ASSOCIATED FUNCTION NODE.
 
-        x_pts = []
-        y_pts = []
-        for i in range(0, 101):
-            x, y = bezier_segment.point_at_t(i / 100)
-            x_pts.append(x)
-            y_pts.append(y)
+            # Top left (Note, y-axis is inverted) (T)
+            aspects_x.append(x-23)
+            aspects_y.append(y-35)
+            aspect_labels.append("T")
+            # Lower z-order prevents lines from going through nodes
+            plt.plot([x-23, x], [y-35, y], color='black', zorder=2, lw=0.5)
 
-        for i in range(len(x_pts)):
-            x_pts[i] = x_pts[i] - 48
+            # Top right (C)
+            aspects_x.append(x+23)
+            aspects_y.append(y-35)
+            aspect_labels.append("C")
+            plt.plot([x+23, x], [y-35, y], color='black', zorder=2, lw=0.5)
 
-        for i in range(len(y_pts)):
-            y_pts[i] = y_pts[i] - 50
+            # Mid left (I)
+            aspects_x.append(x-44)
+            aspects_y.append(y)
+            aspect_labels.append("I")
+            plt.plot([x-44, x], [y, y], color='black', zorder=2, lw=0.5)
 
-        plt.plot(x_pts, y_pts, zorder=1,color='silver')
+            # Mid right (O)
+            aspects_x.append(x+44)
+            aspects_y.append(y)
+            aspect_labels.append("O")
+            plt.plot([x+44, x], [y, y], color='black', zorder=2, lw=0.5)
 
+            # Bottom left (P)
+            aspects_x.append(x-23)
+            aspects_y.append(y+35)
+            aspect_labels.append("P")
+            plt.plot([x-23, x], [y+35, y], color='black', zorder=2, lw=0.5)
+
+            # Bottom right (R)
+            aspects_x.append(x+23)
+            aspects_y.append(y+35)
+            aspect_labels.append("R")
+            plt.plot([x+23, x], [y+35, y], color='black', zorder=2, lw=0.5)
+
+        # Adds aspects to each node
+        plt.scatter(aspects_x, aspects_y, s=30, facecolors='white', edgecolors='black', lw=0.5, zorder=3)
+
+        # Applies proper labels to scatter plot
+        for label, x, y in zip(aspect_labels, aspects_x, aspects_y):
+            wrapped_label = textwrap.fill(label, break_long_words=False, width=1)
+            plt.annotate(wrapped_label, (x, y), ha='center', va='center', fontsize=3.5)
+
+    def bezier_curves(self):
+
+        for i in self.fram_data[2].Curve:
+            # Curve coordinates
+            a = i.split("|")
+
+            # Must be in this order!
+            control_points = [(float(a[2]), float(a[3])), (float(a[4]), float(a[5])), (float(a[8]), float(a[9])), (float(a[6]), float(a[7])), (float(a[0]), float(a[1]))]
+            bezier_segment = BezierSegment(control_points)
+
+            x_pts = []
+            y_pts = []
+            for j in range(0, 101):
+                x, y = bezier_segment.point_at_t(j/100)
+                x_pts.append(x)
+                y_pts.append(y)
+
+            for j in range(len(x_pts)):
+                x_pts[j] = x_pts[j] - 48
+
+            for j in range(len(y_pts)):
+                y_pts[j] = y_pts[j] - 50
+
+            plt.plot(x_pts, y_pts, zorder=1, color='#999999', lw=1)
+
+    def generate(self):
+        self.function_nodes()
+        self.aspects()
+        self.bezier_curves()
+        plt.show()
+
+    def display(self):
+        plt.show()
 
 
 def main():
-    px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
-    plt.figure(figsize=(2433*px, 657*px))
+    test = Visualizer("FRAM model-Stroke care system.xfmv", 2433, 657, 150,'WebAgg')
+    # test = Visualizer("Cup Noodles.xfmv", 1200, 600, 150)
 
-    test = function_nodes()
-    aspects(test[0],test[1],test[2])
-    bezier_curves(test[0])
+    # Does all the work to produce the fram visual, and displays it.
+    test.generate()
 
-    plt.gca().invert_yaxis()
-    plt.axis("off")
-
-    ##Stretches axis to take up entire canvas
-    #plt.tight_layout()
-
-    # Displays copied FRAM in python.
-    plt.show()
-    exit()
 
 if __name__ == "__main__":
     main()
